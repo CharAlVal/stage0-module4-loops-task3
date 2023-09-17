50d1da3fd6b78149412df19a8f1ee0207a3f9a0e
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-09-16 23:46:00 -0600

    fixed

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,11 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-        int result = 9;
+        int result = 0;
         int count = 9;
         for (int i = 1; i <= lengthOfLastNumber; ++i){
             count *= (10) + 9;
             result += count;
+
         }
         System.out.println(result);
 
